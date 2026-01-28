01ee34d7008958477adb08d265a3a9fc9a54e9fa
bhavyakumar-dev <bhavyakumar6840@gmail.com>
2026-01-28 17:08:42 +0530

    Initial commit

---
+++ 1.3 learncpp/1.3 learncpp.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	int x;   // Declare an integer variable named x
+	double width; // Declare a double variable named width
+	int a, b; // Declare two integer variables named a and b with comma separation
+	// int a, int b; // wrong (compiler error)
+	/*int a, double b; // wrong (compiler error)
+
+    int a; double b; // correct (but not recommended)
+
+     // correct and recommended (easier to read)
+     int a;
+     double b;*/
+	return 0;
+}
+
+/*In C++, we use objects to access memory. A named object is called a variable. Each variable has an identifier, a type, and a value (and some other attributes that aren’t relevant here). A variable’s type is used to determine how the value in memory should be interpreted.
+
+Variables are actually created at runtime, when memory is allocated for their use.*/
---
+++ statements-and-the-structure-of-a-program--1.1/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+/* This is a multi-line comment.
+   This line will be ignored.
+   So will this one. */
+
+int main() 
+{
+	std::cout << "Hello! World\n";
+	std::cout << "It is very nice to meet you!\n";
+	std::cout << "Yeahh!\n";
+	std::cout << "1 \n";
+	std::cout << -6.77;
+
+	return 0;
+}
